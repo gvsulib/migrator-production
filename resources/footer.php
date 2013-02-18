6068443fbd5c94f6657dedca187295a40b7fc12b
Adam Luckenbaugh <adam.luckenbaugh@gmail.com>
2013-02-18 13:05:39 -0500

    Added jquery to the project and changed the link in footer.php regarding its inclusion.

--- resources/footer.php
+++ resources/footer.php
@@ -1,6 +1,6 @@
 <?php
 echo '</body>
-	<script type="text/javascript" src="http://'.$SITE_ROOT.'resources/js/jquery.min.js"></script>
+	<script type="text/javascript" src="'.$SITE_ROOT.'resources/js/jquery.min.js"></script>
 	<script type="text/javascript" src="'.$SITE_ROOT.'resources/js/jquery.qtip-1.0.0-rc3.min.js"></script>
 	<script type="text/javascript" src="'.$SITE_ROOT.'resources/js/scripts.js.php"></script>
 	</html>';
