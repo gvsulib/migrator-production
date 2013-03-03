b2cc44331ada21036b1d849e1c6f809ad998edca
Adam Luckenbaugh <adam.luckenbaugh@gmail.com>
2013-03-03 16:32:30 -0500

    Added code to handle the two default initial conditions of the carrier_label field: the original was the numeric "0" and the second, when the type was changed to VARCHAR, was just blank.  We could just edit the database to go to the single empty string case, which would be simpler...

--- exportdb.php
+++ exportdb.php
@@ -4,6 +4,10 @@ require "resources/settings.php";
 require "resources/secret/mysqlconnect.php";
 require "resources/functions.php";
 require "resources/login.php";
+
+//the new line character sequence
+$newline = "\r\n";
+
 if($logged_in){
 	if(isset($_POST['lim'])){
 		//this means the form for downloading was submitted... process it
@@ -19,13 +23,11 @@ if($logged_in){
 	echo 'Access denied.';
 }
 
+/*
+ * Exports a list of the SKUs in the database, one per line.
+ */
 function exportSKUs($exp_limit){
-	global $books_table, $new_entries;
-	$newline = "\r\n";
-	/*
-	 * Dematic needs a text file that line by line has:
-	 * carrier_code,cell_id,sku,dedicated
-	 */
+	global $books_table, $new_entries, $newline;
 	$gzipfn = 'migrator-SKUs-'.date('Y-m-d').'-'.time().'.txt.gzip';
 	$mimeType = 'application/x-gzip';
 	
@@ -52,23 +54,23 @@ function exportSKUs($exp_limit){
 	promptDownload($gzipfn, $mimeType);
 }
 
+/*
+ * This method exports the database in the format Dematic needs.
+ * Dematic needs a text file that line by line has:
+ * carrier_code,cell_id,sku,dedicated
+ */
 function exportdb($exp_limit){
-	global $EXP_DELIM, $books_table, $new_entries;
-	$newline = "\r\n";
-	/*
-	 * Dematic needs a text file that line by line has:
-	 * carrier_code,cell_id,sku,dedicated
-	 */
+	global $EXP_DELIM, $books_table, $new_entries, $newline;
 	$gzipfn = 'migrator-'.date('Y-m-d').'-'.time().'.txt.gzip';
 	$mimeType = 'application/x-gzip';
 	
 	$zh = gzopen($gzipfn,'w');
 	
 	if(!$new_entries){
-		$sql = "SELECT sku, box_code, dedicated, carrier_label, cell_id FROM $books_table WHERE carrier_label != '0'";	//production
+		$sql = "SELECT sku, box_code, dedicated, carrier_label, cell_id FROM $books_table WHERE carrier_label != '' AND carrier_label != '0'";	//production
 	} else {
 		$sql = "SELECT sku, box_code, dedicated, carrier_label, cell_id FROM $books_table WHERE exported = '0'
-			AND carrier_label != '0'";	//production
+			AND carrier_label != '' AND carrier_label != '0'";	//production
 	}
 	if($exp_limit > 0){
 		$sql .= " LIMIT ".$exp_limit;
@@ -81,7 +83,7 @@ function exportdb($exp_limit){
 	gzclose($zh);
 	
 	//mark the books that were just exported as exported in the database
-	mysql_query("UPDATE $books_table SET exported = '1' WHERE exported = '0' AND carrier_label != '0'") or die(mysql_error());
+	mysql_query("UPDATE $books_table SET exported = '1' WHERE exported = '0' AND carrier_label != '0' AND carrier_label != ''") or die(mysql_error());
 	
 	promptDownload($gzipfn, $mimeType);
 }
